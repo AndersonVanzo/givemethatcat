81114e55613dbd4e1a8b1fdd9ccc4338ae34f35d
AndersonVanzo <anderson.vanzo01@gmail.com>
2024-02-11 10:33:49 -0300

    feat: add routes

---
+++ src/routes/main.routes.tsx
@@ -0,0 +1,15 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MainRoutesParamList } from "./types.ts";
+import { HomeController } from "../screens/home/HomeController.tsx";
+import { AboutController } from "../screens/about/AboutController.tsx";
+
+const Tab = createBottomTabNavigator<MainRoutesParamList>();
+
+export const MainRoutes = () => {
+    return (
+        <Tab.Navigator initialRouteName={"Home"}>
+            <Tab.Screen name={"Home"} component={HomeController} />
+            <Tab.Screen name={"About"} component={AboutController} />
+        </Tab.Navigator>
+    );
+};
---
+++ src/routes/types.ts
@@ -0,0 +1,10 @@
+export type MainRoutesParamList = {
+    Home: undefined;
+    About: undefined;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+namespace ReactNavigation {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    type RootParamsList = MainRoutesParamList;
+}
