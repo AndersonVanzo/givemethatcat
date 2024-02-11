bce416b5eae2686fa7dcaef3ec8e9f2ae8d81362
AndersonVanzo <anderson.vanzo01@gmail.com>
2024-02-11 10:12:26 -0300

    chore: changed eslint and prettier configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: "@react-native",
+  rules: {
+    quotes: ["error", "double"],
+    "react/react-in-jsx-scope": ["off"],
+    "react-hooks/exhaustive-deps": ["off"],
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-  arrowParens: 'avoid',
+  arrowParens: "avoid",
   bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+  bracketSpacing: true,
+  printWidth: 100,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 4,
+  trailingComma: "all",
+  useTabs: false,
 };
